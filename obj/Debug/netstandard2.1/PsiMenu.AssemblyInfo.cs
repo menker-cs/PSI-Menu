b9e680c2448886323b1302556112ec91d51c5c6a
NovaDoesThings <huntervye12@gmail.com>
2025-01-18 03:35:15 -0500

    icxdsd
    fsdfsf

--- obj/Debug/netstandard2.1/PsiMenu.AssemblyInfo.cs
+++ obj/Debug/netstandard2.1/PsiMenu.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PsiMenu")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f1c7d9c03d8b58b15d4dd4678e72f2a7de82a43")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4958e7bd1cd7e666e81c40cf453d27c172d5bcaa")]
 [assembly: System.Reflection.AssemblyProductAttribute("PsiMenu")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PsiMenu")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
