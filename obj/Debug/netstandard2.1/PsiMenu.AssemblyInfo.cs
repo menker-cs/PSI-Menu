27b93053e39d5730101fc3b0d191601e4e6600f6
Menker <menker.rizz@gmail.com>
2025-02-06 14:53:43 -0500

    e
    e

--- obj/Debug/netstandard2.1/PsiMenu.AssemblyInfo.cs
+++ obj/Debug/netstandard2.1/PsiMenu.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PsiMenu")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f166029288781b1c84a8962bbb62043afd8f3ec6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+295062c1630eddd5f38c8be50ba8fa7ec2a22ce1")]
 [assembly: System.Reflection.AssemblyProductAttribute("PsiMenu")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PsiMenu")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
